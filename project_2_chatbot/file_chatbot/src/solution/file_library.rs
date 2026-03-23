8ba6b98b471387aad0414c53bdac66cccf1137ee
Jude Makki <jmakki07@bu.edu>
2026-03-23 03:13:46 -0400

    Implement V4 load_chat_session_from_file

--- project_2_chatbot/file_chatbot/src/solution/file_library.rs
+++ project_2_chatbot/file_chatbot/src/solution/file_library.rs
@@ -9,7 +9,7 @@ use std::fs;
 // LlamaChatSession provides helpful functions for loading and storing sessions.
 // Look at https://docs.rs/kalosm/latest/kalosm/language/trait.ChatSession.html#saving-and-loading-sessions
 // for some examples!
-
+  
 // Implement this
 pub fn save_chat_session_to_file(filename: &str, session: &LlamaChatSession) {
     // look at fs::write(...)
@@ -18,7 +18,15 @@ pub fn save_chat_session_to_file(filename: &str, session: &LlamaChatSession) {
 
 // Implement this
 pub fn load_chat_session_from_file(filename: &str) -> Option<LlamaChatSession> {
-    // look at fs::read(...)
+     match fs::read(filename){
+        Ok(bytes) => {match LlamaChatSession::from_bytes(bytes.as_slice()){
+            Ok(session) => Some(session),
+            Err(_)=> None, }
+
+         
+        }
+        Err(_) => None
+    }
+     }
     // also look at LlamaChatSession::from_bytes(...)
     unimplemented!("Loading chat session from file {filename}");
-}

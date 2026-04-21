c2f09931695a9e874cd509e19685f289a110c095
Jude Makki <jmakki07@bu.edu>
2026-04-20 21:04:22 -0400

    Part 2 Step 3: smarter heuristic

--- project_4_tic_tac_toe/tic_tac_toe_5x5/src/solution/agent.rs
+++ project_4_tic_tac_toe/tic_tac_toe_5x5/src/solution/agent.rs
@@ -1,18 +1,88 @@
 use tic_tac_toe_stencil::agents::Agent;
-use tic_tac_toe_stencil::board::Board;
+use tic_tac_toe_stencil::board::{Board, Cell};
 use tic_tac_toe_stencil::player::Player;
 
-// Your solution solution.
+// New improved solution 
 pub struct SolutionAgent {}
 
 impl SolutionAgent {
+    fn heuristic(board: &Board) -> i32 {
+        let mut score = board.score() * 20;
+        let cells = board.get_cells();
+
+        for i in 0..cells.len() {
+            for j in 0..cells.len() {
+                // Row check
+                if j + 2 < cells.len() {
+                    let a = &cells[i][j];
+                    let b = &cells[i][j + 1];
+                    let c = &cells[i][j + 2];
+                    match (a, b, c) {
+                        (Cell::X, Cell::X, Cell::Empty) => score += 1,
+                        (Cell::Empty, Cell::X, Cell::X) => score += 1,
+                        (Cell::X, Cell::Empty, Cell::X) => score += 1,
+                        (Cell::O, Cell::O, Cell::Empty) => score -= 1,
+                        (Cell::Empty, Cell::O, Cell::O) => score -= 1,
+                        (Cell::O, Cell::Empty, Cell::O) => score -= 1,
+                        _ => {}
+                    }
+                }
+                // Column check
+                if i + 2 < cells.len() {
+                    let a = &cells[i][j];
+                    let b = &cells[i + 1][j];
+                    let c = &cells[i + 2][j];
+                    match (a, b, c) {
+                        (Cell::X, Cell::X, Cell::Empty) => score += 1,
+                        (Cell::Empty, Cell::X, Cell::X) => score += 1,
+                        (Cell::X, Cell::Empty, Cell::X) => score += 1,
+                        (Cell::O, Cell::O, Cell::Empty) => score -= 1,
+                        (Cell::Empty, Cell::O, Cell::O) => score -= 1,
+                        (Cell::O, Cell::Empty, Cell::O) => score -= 1,
+                        _ => {}
+                    }
+                }
+                // Diagonal 1
+                if i + 2 < cells.len() && j + 2 < cells.len() {
+                    let a = &cells[i][j];
+                    let b = &cells[i + 1][j + 1];
+                    let c = &cells[i + 2][j + 2];
+                    match (a, b, c) {
+                        (Cell::X, Cell::X, Cell::Empty) => score += 1,
+                        (Cell::Empty, Cell::X, Cell::X) => score += 1,
+                        (Cell::X, Cell::Empty, Cell::X) => score += 1,
+                        (Cell::O, Cell::O, Cell::Empty) => score -= 1,
+                        (Cell::Empty, Cell::O, Cell::O) => score -= 1,
+                        (Cell::O, Cell::Empty, Cell::O) => score -= 1,
+                        _ => {}
+                    }
+                }
+                // Diagonal 2
+                if i + 2 < cells.len() && j >= 2 {
+                    let a = &cells[i][j];
+                    let b = &cells[i + 1][j - 1];
+                    let c = &cells[i + 2][j - 2];
+                    match (a, b, c) {
+                        (Cell::X, Cell::X, Cell::Empty) => score += 1,
+                        (Cell::Empty, Cell::X, Cell::X) => score += 1,
+                        (Cell::X, Cell::Empty, Cell::X) => score += 1,
+                        (Cell::O, Cell::O, Cell::Empty) => score -= 1,
+                        (Cell::Empty, Cell::O, Cell::O) => score -= 1,
+                        (Cell::O, Cell::Empty, Cell::O) => score -= 1,
+                        _ => {}
+                    }
+                }
+            }
+        }
+        score
+    }
+
     fn minimax(board: &mut Board, player: Player, depth: u32, max_depth: u32) -> (i32, usize, usize) {
         if board.game_over() || depth == max_depth {
-            return (board.score(), 0, 0);
+            return (SolutionAgent::heuristic(board), 0, 0);
         }
 
         let avbmoves = board.moves();
-
         let mut best_score = match player {
             Player::X => i32::MIN,
             Player::O => i32::MAX,
@@ -49,9 +119,9 @@ impl SolutionAgent {
 }
 
 impl Agent for SolutionAgent {
-   fn solve(board: &mut Board, player: Player, _time_limit: u64) -> (i32, usize, usize) {
-    let moves = board.moves();
-    let max_depth = if moves.len() <= 9 { moves.len() as u32 } else { 3 };
-    SolutionAgent::minimax(board, player, 0, max_depth)
-}
+    fn solve(board: &mut Board, player: Player, _time_limit: u64) -> (i32, usize, usize) {
+        let cells = board.get_cells();
+        let max_depth = if cells.len() <= 3 { board.moves().len() as u32 } else { 2 };
+        SolutionAgent::minimax(board, player, 0, max_depth)
+    }
 }
